0128be7bdedda359a441e3f5fde9e4ffdc021370
Flore Meyer <floremeyerprevost@gmail.com>
2018-05-24 19:06:56 +0200

    Remove unwanted scenes

--- Assets/Standard Assets/Characters/RollerBall/Scripts/Ball.cs
+++ Assets/Standard Assets/Characters/RollerBall/Scripts/Ball.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 namespace UnityStandardAssets.Vehicles.Ball
 {
+    using CrossPlatformInput;
+
     public class Ball : MonoBehaviour
     {
         [SerializeField] private float m_MovePower = 5; // The force added to the ball to move it.
@@ -12,7 +14,28 @@ namespace UnityStandardAssets.Vehicles.Ball
 
         private const float k_GroundRayLength = 1f; // The length of the ray to check if the ball is grounded.
         private Rigidbody m_Rigidbody;
+        
+        private Vector3 move;
+        // the world-relative desired move direction, calculated from the camForward and user input.
 
+        private Transform cam; // A reference to the main camera in the scenes transform
+        private Vector3 camForward; // The current forward direction of the camera
+        private bool jump; // whether the jump button is currently pressed
+
+        private void Awake()
+        {
+            // get the transform of the main camera
+            if (Camera.main != null)
+            {
+                cam = Camera.main.transform;
+            }
+            else
+            {
+                Debug.LogWarning(
+                    "Warning: no main camera found. Ball needs a Camera tagged \"MainCamera\", for camera-relative controls.");
+                // we use world-relative controls in this case, which may not be what the user wants, but hey, we warned them!
+            }
+        }
 
         private void Start()
         {
@@ -21,6 +44,35 @@ namespace UnityStandardAssets.Vehicles.Ball
             GetComponent<Rigidbody>().maxAngularVelocity = m_MaxAngularVelocity;
         }
 
+        private void Update()
+        {
+            // Get the axis and jump input.
+
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            float v = CrossPlatformInputManager.GetAxis("Vertical");
+            jump = CrossPlatformInputManager.GetButton("Jump");
+
+            // calculate move direction
+            if (cam != null)
+            {
+                // calculate camera relative direction to move:
+                camForward = Vector3.Scale(cam.forward, new Vector3(1, 0, 1)).normalized;
+                move = (v*camForward + h*cam.right).normalized;
+            }
+            else
+            {
+                // we use world-relative directions in the case of no main camera
+                move = (v*Vector3.forward + h*Vector3.right).normalized;
+            }
+        }
+
+
+        private void FixedUpdate()
+        {
+            // Call the Move function of the ball controller
+            Move(move, jump);
+            jump = false;
+        }
 
         public void Move(Vector3 moveDirection, bool jump)
         {
